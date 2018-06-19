3d9d5ede6a5b4dc6665b864a83fab70431b33d4a
Mati <maciejak.dev@gmail.com>
2018-06-19 20:09:43 +0200

    Base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import { ListComponent } from './component/list/list.component';
+import { CreateComponent } from './component/create/create.component';
+import { EditComponent } from './component/edit/edit.component';
+import { ShowComponent } from './component/show/show.component';
+import {routes} from "./routes";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent,
+    ShowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,29 @@
+import {Routes} from "@angular/router";
+import {ListComponent} from "./component/list/list.component";
+import {ShowComponent} from "./component/show/show.component";
+import {EditComponent} from "./component/edit/edit.component";
+import {CreateComponent} from "./component/create/create.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    component: ListComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'edit',
+    component: EditComponent
+  },
+  {
+    path: 'show/:id',
+    component: ShowComponent
+  }
+];
